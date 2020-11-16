e0dd542c7b869a88d1513b6248d7bb1df3ef4b0f
tb <tomek.prezes@gmail.com>
2020-11-16 21:47:54 +0100

    #18 add column position for getting value of read string

---
+++ generate_docs.bat
@@ -0,0 +1,3 @@
+cd ExtendedMainframe3270
+CALL python -m robot.libdoc -f xml  ..\ExtendedMainframe3270 ..\doc\ExtendedMainframew3270.xml
+CALL python -m robot.libdoc -f html  ..\ExtendedMainframe3270 ..\doc\ExtendedMainframew3270.html
